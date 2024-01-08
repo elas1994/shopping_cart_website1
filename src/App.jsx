6839c6b286551b3a281261c3801186a92fe4a7ff
Sale <s_skoric@hotmail.com>
2024-01-08 22:54:33 +0100

    starting setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <a href="/shopping_cart_website1/home">Home</a>
+        <br />
+        <a href="/shopping_cart_website1/about">About</a>
+      </div>
+
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+
+const router123 = createBrowserRouter([
+  {
+    path: "/shopping_cart_website1/",
+    element: <App />,
+    children: [
+      {
+        path: "/shopping_cart_website1/home",
+        element: <Home />,
+      },
+      {
+        path: "/shopping_cart_website1/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router123} />
   </React.StrictMode>
 );
